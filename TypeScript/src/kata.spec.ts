c0a6ee4e31aa18c027a87287dbc67d0942233b73
Jim Kriz <jim@kriz.net>
2023-07-15 14:22:02 -0400

    Minor changes to typescript test

--- TypeScript/src/kata.spec.ts
+++ TypeScript/src/kata.spec.ts
@@ -34,28 +34,31 @@ test('Should be able to create a user and read it back and verify proper values'
 // This is a unit test - we will create a mock of the UserRepository, so that we can isolate the test to
 // only testing the UserService. This will help to track down the bug in the UserService.createUser method.
 test('Should create a user with the proper values', async () => {
+    fail('Please implement this test. Thanks!')
+
     // To Do
+    // 1) Create a mock of the UserRepository
+    // const userRepositoryMock = mock<UserRepository>()
     //
-    // 1) Create a new userService, but "inject" the userRepositoryMock so we can verify what the service sends to the repository `create`
+    // 2) Create a new userService, but "inject" the userRepositoryMock so we can verify what the service sends to the repository `create`
     // const userService = new UserService(instance(userRepositoryMock))
     //
-    // 2) Setup an expectation and action on the user repository mock, so that it will return a response when the `create` method is called
+    // 3) Setup an expectation and action on the user repository mock, so that it will return a response when the `create` method is called
     // const createdUser = new User('first', 'last', new Date(), 123)
     // when(userRepositoryMock.create(anything())).thenResolve(createdUser)
     //
-    // 3) Invoke the service method to add a user
+    // 4) Invoke the service method to add a user
     // const userToCreate = new User('FIRST', 'LAST', new Date())
     // const user = await userService.addUser(userToCreate.lastName, userToCreate.firstName, userToCreate.birthday)
     //
-    // 4) Verify that the expected response was returned from addUser - which should be whatever the repository.create returns
+    // 5) Verify that the expected response was returned from addUser - which should be whatever the repository.create returns
     // expect(user).toBeDefined()
     // expect(user!.firstName).toEqual(createdUser.firstName)
     // expect(user!.lastName).toEqual(createdUser.lastName)
     // expect(user!.birthday).toEqual(createdUser.birthday)
     //
-    // 5) Verify that the repository.create was invoked with proper values. This is the important part, and not possible without mocks
+    // 6) Verify that the repository.create was invoked with proper values. This is the important part, and not possible without mocks
     // verify(userRepositoryMock.create(anything())).once()
     // const createUserRepositoryArgument = capture(userRepositoryMock.create).first()
     // expect(createUserRepositoryArgument[0]).toEqual(userToCreate)
-    fail('Please implement this test. Thanks!')
 })
